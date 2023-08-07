43f9517b7197a0712d22b0a630cc17d4c44f0d94
Edson miguel <gueelmb@gmail.com>
2023-08-07 15:38:04 -0300

    Project creating

---
+++ src/test/java/com/m1guuel/workshop/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m1guuel.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
